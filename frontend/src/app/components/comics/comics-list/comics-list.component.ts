f3c037ce0765a79b8a295394282dec866236ce10
Douglas <emaildedouglas@gmail.com>
2021-05-14 15:55:39 -0300

    6 - Implementação do formulário do cliente e emissão de snackbar com status

--- frontend/src/app/components/comics/comics-list/comics-list.component.ts
+++ frontend/src/app/components/comics/comics-list/comics-list.component.ts
@@ -14,36 +14,21 @@ import { ComicViewComponent } from '../comic-view/comic-view.component';
 })
 export class ComicsListComponent implements OnInit {
   
-  allCharacters: any[];
-  slides = [
-    'First slide',
-    'Second slide',
-    'Third slide',
-    'Fourth slide',
-    'Fifth slide',
-    'Sixth slide'
-  ];
+  comics: any[];
+
   constructor(public service: ComicsService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.getCharacters();
-   /* this.print({id: 94483, title: "Fantastic Four (2018) #32 (Variant)", issueNumber: 32, path: "Variant", thumbnail:{path: 'http://i.annihil.us/u/prod/marvel/i/mg/7/10/6092ed44c99a8', extension:'jpg'}, creators:{items: [{resourceURI: "http://gateway.marvel.com/v1/public/creators/11482", name: "Jesus Aburtov", role: "colorist"},
-     {resourceURI: "http://gateway.marvel.com/v1/public/creators/5251", name: "Vc Joe Caramagna", role: "letterer"},
-     {resourceURI: "http://gateway.marvel.com/v1/public/creators/13127", name: "Emily Newcomen", role: "editor"},
-     {resourceURI: "http://gateway.marvel.com/v1/public/creators/12489", name: "R.B. Silva", role: "inker"},
-     {resourceURI: "http://gateway.marvel.com/v1/public/creators/12983", name: "Dan Slott", role: "writer"},
-     {resourceURI: "http://gateway.marvel.com/v1/public/creators/7190", name: "Skottie Young", role: "painter (cover)"}]},
-    prices: [{price: 3.99}]})*/
   }
 
   getCharacters() {
     this.service.listarQuadrinhos().subscribe(data => {
-      this.allCharacters = data.data.results;
+      this.comics = data.data.results;
     });
   }
 
   print(event){
-    console.log(event);
     this.dialog.open(ComicViewComponent, {
       data: {
         title: event.title,
@@ -56,13 +41,7 @@ export class ComicsListComponent implements OnInit {
     });
   }
   
-  onSwiper(swiper) {
-    console.log(swiper);
-  }
-  onSlideChange() {
-    console.log('slide change');
-  }
-
+  
   public config: SwiperOptions = {
     direction: 'horizontal',
     slidesPerView: 5,
--- frontend/src/app/components/maps/map-view/map-view.component.ts
+++ frontend/src/app/components/maps/map-view/map-view.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {Appearance, GermanAddress, Location} from '@angular-material-extensions/google-maps-autocomplete';
 import PlaceResult = google.maps.places.PlaceResult;
 import {Title} from '@angular/platform-browser';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-map-view',
@@ -15,15 +18,19 @@ export class MapViewComponent implements OnInit {
   public latitude: number;
   public longitude: number;
   public selectedAddress: PlaceResult;
-  constructor(private titleService: Title) {
+  public form: FormGroup;
+  
+  constructor(private titleService: Title, private fb: FormBuilder, public dialogRef: MatDialogRef<MapViewComponent>,
+    private _snackBar: MatSnackBar) {
   }
 
   ngOnInit() {
+    this.createForm();
     this.titleService.setTitle('Home | @angular-material-extensions/google-maps-autocomplete');
 
     this.zoom = 10;
-    this.latitude = -8.1688884;
-    this.longitude = -34.9281449;
+    this.latitude = -8.0631633;
+    this.longitude = -34.8711337;
 
     this.setCurrentPosition();
 
@@ -40,18 +47,45 @@ export class MapViewComponent implements OnInit {
   }
 
   onAutocompleteSelected(result: PlaceResult) {
-    console.log('onAutocompleteSelected: ', result);
+    this.form.get('address').setValue(result.address_components);
+    this.form.get('formatedAddress').setValue(result.formatted_address);
+
   }
 
   onLocationSelected(location: Location) {
-    console.log('onLocationSelected: ', location);
     this.latitude = location.latitude;
     this.longitude = location.longitude;
     this.zoom = 15;
   }
 
-  onGermanAddressMapped($event) {
-    console.log('onGermanAddressMapped', $event);
+
+  createForm(){
+    this.form = this.fb.group({
+      name: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
+      age: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
+      cpf: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      agm: new FormControl(null,[Validators.required, Validators.maxLength(10)]),
+      address: new FormControl(''),
+      formatedAddress: new FormControl('')
+    })
+  }
+
+  onSubmit(){
+    this.onClose();
+    this.openSnackBar();
+  }
+
+  onClose(){
+    this.dialogRef.close();
+  }
+
+  openSnackBar() {
+    this._snackBar.open('Quadrinho encomendado com sucesso!','Fechar', {
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      duration: 5000,
+    });
   }
 
 }
